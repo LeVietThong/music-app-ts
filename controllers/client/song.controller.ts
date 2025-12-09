6f901034f142443bf50945957c81c15fa74d4fce
ThongLV <levietthong92@gmail.com>
2025-12-09 16:06:37 +0700

    trang chi tiết bài hát

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -31,3 +31,33 @@ export const list = async (req: Request, res: Response) => {
     songs: songs,
   });
 };
+
+//[GET] /songs/detail/:slugSong
+export const detail = async (req: Request, res: Response) => {
+  const slugSong: string = req.params.slugSong;
+
+  const song = await Song.findOne({
+    slug: slugSong,
+    deleted: false,
+    status: "active",
+  });
+
+  const singer = await Singer.findOne({
+    _id: song.singerId,
+    deleted: false,
+    status: "active",
+  }).select("fullName");
+
+  const topic = await Topic.findOne({
+    _id: song.topicId,
+    deleted: false,
+    status: "active",
+  }).select("title");
+
+  res.render("client/pages/songs/detail", {
+    pageTitle: song.title,
+    song: song,
+    singer: singer,
+    topic: topic,
+  });
+};
---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+const aplayer = document.querySelector("#aplayer");
+
+if (aplayer) {
+  let dataSong = aplayer.getAttribute("data-song");
+  dataSong = JSON.parse(dataSong);
+
+  let dataSinger = aplayer.getAttribute("data-singer");
+  dataSinger = JSON.parse(dataSinger);
+
+  const ap = new APlayer({
+    container: aplayer,
+    audio: [
+      {
+        name: dataSong.title,
+        artist: dataSinger.fullName,
+        url: dataSong.audio,
+        cover: dataSong.avatar,
+      },
+    ],
+    autoplay: true,
+  });
+
+  const avatar = document.querySelector(".singer-detail .inner-avatar");
+
+  ap.on("play", function () {
+    avatar.style.animationiconPlayState = "running";
+  });
+
+  ap.on("pause", function () {
+    avatar.style.animationiconPlayState = "paused";
+  });
+}
