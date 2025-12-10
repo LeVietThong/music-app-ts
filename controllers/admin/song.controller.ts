51f0c7704d6dffa7f3739cf87019631d4d71f507
ThongLV <levietthong92@gmail.com>
2025-12-10 14:32:34 +0700

    làm giao diện

--- controllers/admin/song.controller.ts
+++ controllers/admin/song.controller.ts
@@ -1,16 +1,37 @@
 import { Request, Response } from "express";
 import Song from "../../models/song.model";
+import Topic from "../../models/topic.model";
+import Singer from "../../models/singer.model";
 
 // [GET] /admin/songs/
 export const index = async (req: Request, res: Response) => {
   const songs = await Song.find({
-    deleted: false
+    deleted: false,
   });
 
   // console.log(songs);
 
   res.render("admin/pages/songs/index", {
     pageTitle: "Quản lý bài hát",
-    songs: songs
+    songs: songs,
+  });
+};
+
+// [GET] /admin/songs/create
+export const create = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    status: "active",
+    deleted: false,
+  }).select("title");
+
+  const singers = await Singer.find({
+    status: "active",
+    deleted: false,
+  }).select("fullName");
+
+  res.render("admin/pages/songs/create", {
+    pageTitle: "Thêm mới bài hát",
+    topics: topics,
+    singers: singers,
   });
 };
