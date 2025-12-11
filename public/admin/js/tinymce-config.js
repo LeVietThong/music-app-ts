ce817747200bb76a7aaa3696db9d9557a5abc94f
ThongLV <levietthong92@gmail.com>
2025-12-11 09:13:59 +0700

    upload ảnh từ tinymce lên cloud

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -7,27 +7,29 @@ tinymce.init({
   file_picker_types: 'image',
   license_key: 'gpl', // ✅ dòng này là bắt buộc với bản npm
 
-  file_picker_callback: function (cb, value, meta) {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
+  images_upload_url: '/admin/upload'
 
-    input.onchange = function () {
-      const file = this.files[0];
-      const reader = new FileReader();
+  // file_picker_callback: function (cb, value, meta) {
+  //   const input = document.createElement('input');
+  //   input.setAttribute('type', 'file');
+  //   input.setAttribute('accept', 'image/*');
 
-      reader.onload = function () {
-        const id = 'blobid' + new Date().getTime();
-        const blobCache = tinymce.activeEditor.editorUpload.blobCache;
-        const base64 = reader.result.split(',')[1];
-        const blobInfo = blobCache.create(id, file, base64);
-        blobCache.add(blobInfo);
-        cb(blobInfo.blobUri(), { title: file.name });
-      };
+  //   input.onchange = function () {
+  //     const file = this.files[0];
+  //     const reader = new FileReader();
 
-      reader.readAsDataURL(file);
-    };
+  //     reader.onload = function () {
+  //       const id = 'blobid' + new Date().getTime();
+  //       const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+  //       const base64 = reader.result.split(',')[1];
+  //       const blobInfo = blobCache.create(id, file, base64);
+  //       blobCache.add(blobInfo);
+  //       cb(blobInfo.blobUri(), { title: file.name });
+  //     };
 
-    input.click();
-  }
+  //     reader.readAsDataURL(file);
+  //   };
+
+  //   input.click();
+  // }
 });
